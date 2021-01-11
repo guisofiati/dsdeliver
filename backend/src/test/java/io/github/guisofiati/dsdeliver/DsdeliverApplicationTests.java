393e020fafee24f6a769361a96210e06739a62c5
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-01-11 16:09:48 -0300

    Project created

---
+++ backend/src/test/java/io/github/guisofiati/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.guisofiati.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
