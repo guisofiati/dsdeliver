0486b62ebc6c8442d61dbb99b35db45eecad0c44
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-04-25 17:27:40 -0300

    Search order location

--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from '../api';
+import { OrderLocationData } from './types';
 
 const initialPosition = {
     lat: -23.5623874,
@@ -17,7 +18,11 @@ type Place = {
     }
 }
 
-function OrderLocation() {
+type Props = {
+    onChangeLocation: (location: OrderLocationData) => void;
+}
+
+function OrderLocation({ onChangeLocation }: Props) {
 
     const [address, setAddress] = useState<Place>({
         position: initialPosition
@@ -33,8 +38,7 @@ function OrderLocation() {
             position: {
               lat: item.center[1],
               lng: item.center[0]
-            },
-            place: item.place_name,
+            }
           });
         });
       
@@ -43,11 +47,11 @@ function OrderLocation() {
       
       const handleChangeSelect = (place: Place) => {
         setAddress(place);
-        //onChangeLocation({
-          //latitude: place.position.lat,
-          //longitude: place.position.lng,
-          //address: place.label!
-        //});
+        onChangeLocation({
+          latitude: place.position.lat,
+          longitude: place.position.lng,
+          address: place.label!
+        });
       };
 
       return (
